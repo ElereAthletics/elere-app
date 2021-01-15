8fa2b832809d4774839e4fce284f8352eaed1f7b
Tasin Ahmed <atasin1998@gmail.com>
2021-01-14 22:12:24 -0500

    Added route loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import { Fragment, useEffect, useState } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
-
 import { GlobalStyle, theme } from '../style'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+
+NProgress.configure({ showSpinner: false })
+
+Router.events.on('routeChangeStart', () => {
+	NProgress.start()
+})
+
+Router.events.on('routeChangeComplete', () => {
+	NProgress.done()
+})
+
+Router.events.on('routeChangeError', () => {
+	NProgress.done()
+})
 
 const App = ({ Component, pageProps }: AppProps) => {
 	const [isMounted, setIsMounted] = useState(false)
